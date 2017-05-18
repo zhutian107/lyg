837edf3c2e4df0eeef16bbfb212017d40feffe45
liuyaguang107 <1239742815@qq.com>
2017-05-18 22:14:57 +0800

    刘亚光02

--- src/js/index.js
+++ src/js/index.js
@@ -85,7 +85,7 @@ for (var i = 0; i < lis.length; i++) {
                 for (var j = 0; j < divs.length; j++) {
                     $('.divs div').eq(j).hide();
                 }
-                $('.divs div').eq(this.index).show();
+                $('.divs div').eq(this.index).show(1000);
                 for (var k = 0; k < lis.length; k++) {
                     lis[k].style.background="";
                 }
@@ -94,6 +94,23 @@ for (var i = 0; i < lis.length; i++) {
             }
           }
         lis[0].style.background="white";
+var lis_1 = document.querySelectorAll('.spans_1 span');
+var divs_1 = document.querySelectorAll('.divs_1 div');
+for (var i = 0; i < lis_1.length; i++) {
+            lis_1[i].index = i;
+            lis_1[i].onmouseover = function(){
+                for (var j = 0; j < divs_1.length; j++) {
+                    $('.divs_1 div').eq(j).hide(500);
+                }
+                $('.divs_1 div').eq(this.index).show();
+                for (var k = 0; k < lis_1.length; k++) {
+                    lis_1[k].style.background="";
+                }
+                this.style.background="green";
+
+            }
+          }
+        lis_1[0].style.background="green";
 ajax.ajaxFn({
     url:"data/01.json",
     data:{
@@ -105,8 +122,28 @@ ajax.ajaxFn({
         var data = data.data.list[0];
         // console.log(data); 
         for(var i in data){
-        	var ul = $('<ul><li><img src="'+data[i].pic+'"><p>'+data[i].title+'</p><p><span>'+data[i].cn_name+'</span><span>'+data[i].id+'</span></p></li></ul>');
+        	var ul = $('<ul><li><div id="mask"></div><img src="'+data[i].pic+'"><p>'+data[i].title+'</p><p><span>'+data[i].cn_name+'</span><span>'+data[i].id+'</span></p></li></ul>');
             ul.appendTo($('.imgs'));
+        }
+        for(var ii in data){
+            var ul = $('<ul><li><img src="'+data[ii].pic+'"><p>'+data[ii].title+'</p><p><span>'+data[ii].cn_name+'</span><span>'+data[ii].id+'</span></p></li></ul>');
+            ul.appendTo($('#sw_5'));
+        }
+        for(var iii in data){
+            var ul = $('<ul><li><img src="'+data[iii].pic+'"><p>'+data[iii].title+'</p><p><span>'+data[iii].cn_name+'</span><span>'+data[iii].id+'</span></p></li></ul>');
+            ul.appendTo($('#sw_6'));
+        }
+        for(var iiii in data){
+            var ul = $('<ul><li><img src="'+data[iiii].pic+'"><p>'+data[iiii].title+'</p><p><span>'+data[iiii].cn_name+'</span><span>'+data[iiii].id+'</span></p></li></ul>');
+            ul.appendTo($('#sw_7'));
+        }
+        for(var iiiii in data){
+            var ul = $('<ul><li><img src="'+data[iiiii].pic+'"><p>'+data[iiiii].title+'</p><p><span>'+data[iiiii].cn_name+'</span><span>'+data[iiiii].id+'</span></p></li></ul>');
+            ul.appendTo($('#sw_8'));
+        }
+        for(var iiiiii in data){
+            var ul = $('<ul><li><img src="'+data[iiiiii].pic+'"><p>'+data[iiiiii].title+'</p><p><span>'+data[iiiiii].cn_name+'</span><span>'+data[iiiiii].id+'</span></p></li></ul>');
+            ul.appendTo($('#sw_9'));
         }
     },
     errorFn:function(err){
@@ -131,23 +168,35 @@ ajax.ajaxFn({
     errorFn:function(err){
     }
 });
-// ajax.ajaxFn({
-//     url:"data/shangcheng02.json",
-//     data:{
+ajax.ajaxFn({
+    url:"data/shangcheng02.json",
+    data:{
         
-//     },
-//     type:'get',
-//     successFn:function(data){
-//         var data = JSON.parse(data);
-//         console.log(data); 
-//         for(var i in data){
-//             var dl = $('<dl class="dl"><dt><img src="'+data[i].a+'"></dt><dd><a href="#"'+data[i].b+'</a><p><span class="btn btn-default">'+data[i].c+'</span><span class="long_1">'+data[i].d+'</span></p><p class="dd_2"><mark>'+data[i].e+'</mark>元起</p></dd></dl>');
-//             dl.appendTo($('#sw_1'));
-//         }
-//     },
-//     errorFn:function(err){
-//     }
-// });
+    },
+    type:'get',
+    successFn:function(data){
+        var data = JSON.parse(data);
+        console.log(data); 
+        for(var i in data){
+            var dl = $('<dl class="dl"><dt><img src="'+data[i].a+'"></dt><dd><a href="#">'+data[i].b+'</a><p><span class="btn btn-default">'+data[i].c+'</span><span class="long_1">'+data[i].d+'</span></p><p class="dd_2"><mark>'+data[i].e+'</mark>元起</p></dd></dl>');
+            dl.appendTo($('#sw_1'));
+        }
+        for(var k in data){
+            var dl = $('<dl class="dl"><dt><img src="'+data[k].a+'"></dt><dd><a href="#">'+data[k].b+'</a><p><span class="btn btn-default">'+data[k].c+'</span><span class="long_1">'+data[k].d+'</span></p><p class="dd_2"><mark>'+data[k].e+'</mark>元起</p></dd></dl>');
+            dl.appendTo($('#sw_2'));
+        }
+        for(var kk in data){
+            var dl = $('<dl class="dl"><dt><img src="'+data[kk].a+'"></dt><dd><a href="#">'+data[kk].b+'</a><p><span class="btn btn-default">'+data[kk].c+'</span><span class="long_1">'+data[kk].d+'</span></p><p class="dd_2"><mark>'+data[kk].e+'</mark>元起</p></dd></dl>');
+            dl.appendTo($('#sw_3'));
+        }
+        for(var kkk in data){
+            var dl = $('<dl class="dl"><dt><img src="'+data[kkk].a+'"></dt><dd><a href="#">'+data[kkk].b+'</a><p><span class="btn btn-default">'+data[kkk].c+'</span><span class="long_1">'+data[kkk].d+'</span></p><p class="dd_2"><mark>'+data[kkk].e+'</mark>元起</p></dd></dl>');
+            dl.appendTo($('#sw_4'));
+        }
+    },
+    errorFn:function(err){
+    }
+});
 
         
         
